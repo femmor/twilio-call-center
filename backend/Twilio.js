abfd53d53af2c3f68d343680638c6a02e0088da8
Emmanuel Egomson <fegomson@gmail.com>
2022-09-30 17:35:05 -0500

    Starting up express server

--- backend/Twilio.js
+++ backend/Twilio.js
@@ -3,6 +3,7 @@ const PHONE_NUMBER_SID = process.env.PHONE_NUMBER_SID;
 const TOKEN_SID = process.env.TOKEN_SID;
 const TOKEN_SECRET = process.env.TOKEN_SECRET;
 const ACCOUNT_SID = process.env.ACCOUNT_SID;
+const VERIFY = process.env.VERIFY;
 
 module.exports = {
   PHONE_NUMBER,
@@ -10,4 +11,5 @@ module.exports = {
   TOKEN_SID,
   TOKEN_SECRET,
   ACCOUNT_SID,
+  VERIFY,
 };
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,28 @@
 const dotenv = require('dotenv');
 dotenv.config();
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
 
+// Service tokens
 const { PHONE_NUMBER } = require('./Twilio');
 
-console.log(PHONE_NUMBER);
+// Initialize the app
+const app = express();
+
+// Middleware
+app.use(cors());
+app.options('*', cors());
+app.use(express.json());
+app.use(morgan('dev'));
+
+// Routes
+app.get('/home', (req, res) => {
+  res.send('Welcome to Twilio!');
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
